540e793480a0b5b939dc79a345121e3553fbfde8
Joel Nickson <joel2000nickson@gmail.com>
2020-08-25 18:42:08 +0530

    Creating my first js assignment

---
+++ site/mod4/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+// DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+(function(window) {
+
+	var byeSpeaker = {};
+
+	var speakWord = "Good Bye";
+
+	byeSpeaker.speak =function(name) {
+	
+	  console.log(speakWord + " " + name);
+	}
+
+	window.byeSpeaker=byeSpeaker;
+
+})(window);
---
+++ site/mod4/SpeakHello.js
@@ -0,0 +1,15 @@
+// DO NOT attach the speakWord variable to the 'helloSpeaker' object.
+(function (window) {
+	
+	var helloSpeaker ={};
+
+	var speakWord = "Hello";
+
+	helloSpeaker.speak=function (name) {
+
+	  console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker= helloSpeaker;
+
+})(window);
